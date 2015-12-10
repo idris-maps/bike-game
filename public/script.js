07a2555bc5f9259da8db160d865818c2a2c40e26
anders <anders@hardabud.com>
2015-12-10 17:34:51 +0100

    canvas text

--- public/script.js
+++ public/script.js
@@ -43,6 +43,19 @@ module.exports = function() {
 		width: 0,
 		height: 0
 	})
+/*
+	canvas.add({
+		id: 'text',
+		type: 'text',
+		cl: 'text',
+		text: 'hello',
+		fontFamily: 'Comic Sans MS',
+		fontSize: 30,
+		x: 50,
+		y: 50,
+		fill: 'red'
+	})
+*/
 }
 
 },{"./canvas":4}],2:[function(require,module,exports){
@@ -153,15 +166,19 @@ exports.add = function(config) {
 		id: config.id,
 		type: config.type,
 		x: config.x,
-		y: config.y,
-		width: config.width,
-		height: config.height
+		y: config.y
 	}
+	if(config.width !== undefined) { newObj.width = config.width }
+	if(config.height !== undefined) { newObj.height = config.height }
 	if(config.elId !== undefined) { newObj.elId = config.elId }
 	if(config.fill !== undefined) { newObj.fill = config.fill }
 	if(config.stroke !== undefined) { newObj.stroke = config.stroke }
 	if(config.cl !== undefined) { newObj.cl = config.cl }
 	if(config.lane !== undefined) { newObj.lane = config.lane }
+	if(config.text !== undefined) { newObj.text = config.text }
+	if(config.opacity !== undefined) { newObj.opacity = config.opacity }
+	if(config.fontFamily !== undefined) { newObj.fontFamily = config.fontFamily }
+	if(config.fontSize !== undefined) { newObj.fontSize = config.fontSize }
 	window.config.objs.push(newObj)
 }
 
@@ -220,6 +237,13 @@ exports.draw = function() {
 	var objs = window.config.objs
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	for(i=0;i<objs.length;i++) {
+
+		if(objs[i].opacity === undefined) {
+			ctx.globalAlpha = 1
+		} else {
+			ctx.globalAlpha = objs[i].opacity
+		}
+
 		if(objs[i].type === 'image') {
 			var img = document.getElementById(objs[i].elId)
 			ctx.drawImage(img, objs[i].x, objs[i].y, objs[i].width, objs[i].height)	
@@ -233,6 +257,21 @@ exports.draw = function() {
 			if(objs[i].stroke !== undefined) {
 				ctx.strokeStyle = objs[i].stroke
 				ctx.stroke()
+			}
+		} else if(objs[i].type === 'text') {
+			if(objs[i].text === undefined) { var txt = 'text' } else { var txt = objs[i].text }
+			if(objs[i].fontSize === undefined) { var fSize = 10 } else { var fSize = objs[i].fontSize }
+			if(objs[i].fontFamily === undefined) { var fFam = 'Arial' } else { var fFam = objs[i].fontFamily }
+			ctx.beginPath()
+			ctx.font = fSize.toString() + 'px ' + fFam
+			if(objs[i].fill !== undefined) {
+				ctx.fillStyle = objs[i].fill
+				ctx.fillText(txt, objs[i].x, objs[i].y)
+			}
+			if(objs[i].stroke !== undefined) {
+				ctx.strokeStyle = objs[i].stroke
+				ctx.stroke()
+				ctx.strokeText(txt, objs[i].x, objs[i].y)
 			}
 		}
 	}
